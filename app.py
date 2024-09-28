5ecb86211ca4d4d3470b669064b4bb33040b3102
suzukaotto <me@szk.kr>
2024-09-28 10:18:05 +0900

    yaa

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import sqlite3
 import base64
+import os
+import time
 
 conn = sqlite3.connect('images.sqlite')
 
@@ -7,5 +9,13 @@ c = conn.cursor()
 c.execute('SELECT * FROM images')
 rows = c.fetchall()
 
+def reftime(str_ftime, re_format='%Y%m%d_%H%M'):
+    return time.strftime(re_format, time.strptime(str_ftime, '%Y-%m-%d %H:%M'))
+
+SAVE_PATH = os.path.join(os.getcwd(), 'images')
+if not os.path.exists(SAVE_PATH):
+    os.makedirs(SAVE_PATH)
+
 for index, row in enumerate(rows):
-    print(f"{index}. {row[0]}")
+    with open(os.path.join(SAVE_PATH, f"{reftime(row[1])}.jpg"), 'wb') as f:
+        f.write(base64.b64decode(row[2]))
